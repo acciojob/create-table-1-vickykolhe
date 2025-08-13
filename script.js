fb8c34bc14d90f841d91de48ba757912bcaabd49
Babusha Kolhe <165826953+vickykolhe@users.noreply.github.com>
2025-08-14 04:23:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
+	let tab=document.getElementById("sampleTable");
+	 let newRow = tab.insertRow(0); 
+
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
   
   
 }
